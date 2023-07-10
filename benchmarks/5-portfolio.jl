1d16294a595922fe2e4ea8fd3886c51f745e530d
Theo Diamandis <tjdiamandis@gmail.com>
2023-07-09 20:51:19 -0400

    update portfolio

--- benchmarks/5-portfolio.jl
+++ benchmarks/5-portfolio.jl
@@ -3,47 +3,18 @@ Pkg.activate(@__DIR__)
 using Random, LinearAlgebra, SparseArrays, Printf
 using Plots, LaTeXStrings
 using Statistics, JLD2
+using IterativeSolvers, LinearMaps
+using COSMO, OSQP
 include(joinpath(@__DIR__, "utils.jl"))
 
 Pkg.activate(joinpath(@__DIR__, ".."))
 using GeNIOS
 
-SAVEPATH = joinpath(@__DIR__, "saved")
-SAVEFILE = joinpath(SAVEPATH, "5-portfolio.jld2")
-FIGS_PATH = joinpath(@__DIR__, "figures")
+const SAVEPATH = joinpath(@__DIR__, "saved", "5-portfolio")
+const FIGS_PATH = joinpath(@__DIR__, "figures")
+const RAN_TRIALS = false
 
-
-## Generate the problem data
-Random.seed!(1)
-k = 10
-n = 100k
-# Σ = F*F' + diag(d)
-F = sprandn(n, k, 0.5)
-d = rand(n) * sqrt(k)
-μ = randn(n)
-γ = 1;
-
-
-## Vanilla QP
-P = γ*(F*F' + Diagonal(d))
-q = -μ
-M = vcat(I, ones(1, n))
-l = vcat(zeros(n), ones(1))
-u = vcat(Inf*ones(n), ones(1))
-
-# compile
-solve!(GeNIOS.QPSolver(P, q, M, l, u); options=GeNIOS.SolverOptions(max_iters=2))
-
-# solve
-solver = GeNIOS.QPSolver(P, q, M, l, u)
-options = GeNIOS.SolverOptions(
-    num_threads=Sys.CPU_THREADS,
-)
-GC.gc()
-result_qp = solve!(solver; options=options)
-
-
-## QP with custom operators
+# FOR QP SOLVER (custom operators)
 # P = γ*(F*F' + Diagonal(d))
 struct FastP
     F
@@ -58,7 +29,6 @@ function LinearAlgebra.mul!(y::AbstractVector, P::FastP, x::AbstractVector)
     @. y *= P.γ
     return nothing
 end
-P = FastP(F, d, γ, zeros(k))
 
 # M = vcat(I, ones(1, n))
 struct FastM 
@@ -80,34 +50,8 @@ function LinearAlgebra.mul!(x::AbstractVector{T}, M::Adjoint{T, FastM}, y::Abstr
     @. x = α * ( y[1:M.parent.n] + y[end] ) + β * x
     return nothing
 end
-M = FastM(n)
-solver = GeNIOS.QPSolver(P, q, M, l, u);
-GC.gc()
-result_op = solve!(solver; options=options)
 
-
-## Generic interface with custom f and g 
-# f(x) = γ/2 xᵀ(FFᵀ + D)x - μᵀx
-function f(x, F, d, μ, γ, tmp)
-    mul!(tmp, F', x)
-    qf = sum(w->w^2, tmp)
-    qf += sum(i->d[i]*x[i]^2, 1:n)
-
-    return γ/2 * qf - dot(μ, x)
-end
-f(x) = f(x, F, d, μ, γ, zeros(k))
-
-#  ∇f(x) = γ(FFᵀ + D)x - μ
-function grad_f!(g, x, F, d, μ, γ, tmp)
-    mul!(tmp, F', x)
-    mul!(g, F, tmp)
-    @. g += d*x
-    @. g *= γ
-    @. g -= μ
-    return nothing
-end
-grad_f!(g, x) = grad_f!(g, x, F, d, μ, γ, zeros(k))
-
+# FOR GENERIC SOLVER
 # ∇²f(x) = γ(FFᵀ + D)
 struct HessianMarkowitz{T, S <: AbstractMatrix{T}} <: HessianOperator
     F::S
@@ -117,121 +61,288 @@ end
 function LinearAlgebra.mul!(y, H::HessianMarkowitz, x)
     mul!(H.vk, H.F', x)
     mul!(y, H.F, H.vk)
-    @. y += d*x
+    @. y += H.d*x
     return nothing
 end
-Hf = HessianMarkowitz(F, d, zeros(k))
 
-# g(z) = I(z)
-function g(z)
-    T = eltype(z)
-    return all(z .>= zero(T)) && abs(sum(z) - one(T)) < 1e-6 ? 0 : Inf
+function generate_portfolio_data(n, k; rseed=1)
+    Random.seed!(1)
+
+    # Σ = F*F' + diag(d)
+    F = sprandn(n, k, 0.5)
+    d = rand(n) * sqrt(k)
+    μ = randn(n)
+    γ = 1;
+    return F, d, μ, γ
 end
-function prox_g!(v, z, ρ)
-    z_max = maximum(w->abs(w), z)
-    l = -z_max - 1
-    u = z_max
 
-    # bisection search to find zero of F
-    while u - l > 1e-8
-        m = (l + u) / 2
-        if sum(w->max(w - m, zero(eltype(z))), z) - 1 > 0
-            l = m
-        else
-            u = m
-        end
+function build_qp_matrices(F, d, μ, γ)
+    n, k = size(F)
+    P = γ * blockdiag(spdiagm(d), spdiagm(ones(k)))
+    q = vcat(-μ, zeros(k))
+    A = [
+        F'                  -spdiagm(ones(k));
+        spdiagm(ones(n))    spzeros(n, k)
+        ones(1, n)          zeros(1, k)
+    ]
+    l = vcat(zeros(k), zeros(n), 1)
+    u = vcat(zeros(k), Inf*ones(n), 1)
+
+    return P, q, A, l, u
+end
+
+function run_trial(n::Int; solvers=[:qp, :op, :custom, :cosmo_indirect, :cosmo_direct, :osqp])
+    GC.gc()
+    BLAS.set_num_threads(1)
+    filename = "portfolio-genios-$n.jld2"
+    savefile = joinpath(SAVEPATH, filename)
+
+    k = n ÷ 50
+    F, d, μ, γ = generate_portfolio_data(n, k)
+    P_eq, q_eq, M_eq, l_eq, u_eq = build_qp_matrices(F, d, μ, γ)
+    GC.gc()
+
+
+    # OSQP
+    if :osqp ∈ solvers
+        osqp_model = OSQP.Model()
+        OSQP.setup!(
+            osqp_model; P=P_eq, q=q_eq, A=M_eq, l=l_eq, u=u_eq, 
+            eps_abs=1e-4, eps_rel=1e-4, verbose=false, time_limit=600,
+        )
+        result_osqp = OSQP.solve!(osqp_model)
+    else
+        result_osqp = nothing
     end
-    ν = (l + u) / 2
-    @. v = max(z - ν, zero(eltype(z)))
+
+
+    # COSMO (indirect)
+    if :cosmo_indirect ∈ solvers
+        model_cosmo_indirect = COSMO.Model()
+        cs1 = COSMO.Constraint(M_eq, zeros(n+k+1), COSMO.Box(l_eq, u_eq))
+        settings = COSMO.Settings(
+            kkt_solver=CGIndirectKKTSolver, 
+            verbose=false,
+            verbose_timing = true,
+            eps_abs=1e-4,
+            eps_rel=1e-4,
+            time_limit=600,
+        )
+        assemble!(model_cosmo_indirect, P_eq, q_eq, cs1, settings=settings)
+        GC.gc()
+        result_cosmo_indirect = COSMO.optimize!(model_cosmo_indirect)
+    else
+        result_cosmo_indirect = nothing
+    end
+
+
+    # COSMO
+    if :cosmo_direct ∈ solvers
+        model_cosmo_direct = COSMO.Model()
+        cs1 = COSMO.Constraint(M_eq, zeros(n+k+1), COSMO.Box(l_eq, u_eq))
+        settings = COSMO.Settings(
+            kkt_solver=QdldlKKTSolver,
+            verbose=false,
+            verbose_timing = true,
+            eps_abs=1e-4,
+            eps_rel=1e-4,
+            time_limit=600,
+        )
+        assemble!(model_cosmo_direct, P_eq, q_eq, cs1, settings=settings)
+        GC.gc()
+        result_cosmo_direct = COSMO.optimize!(model_cosmo_direct)
+    else
+        result_cosmo_direct = nothing
+    end
+
+
+    # GeNIOS
+    options = GeNIOS.SolverOptions(
+        verbose=false,
+        eps_abs=1e-4,
+        eps_rel=1e-4,
+        sketch_update_iter=10_000,
+        num_threads=1,
+        max_time_sec=600.0,
+    )
+
+    if :qp ∈ solvers
+        solver = GeNIOS.QPSolver(P_eq, q_eq, M_eq, l_eq, u_eq)
+        GC.gc()
+        result_qp = solve!(solver; options=options)
+    else
+        result_qp = nothing
+    end
+
+
+    # QP with custom operators
+    if :op ∈ solvers
+        P = FastP(F, d, γ, zeros(k))
+        M = FastM(n)
+        q = -μ
+        l = vcat(zeros(n), ones(1))
+        u = vcat(Inf*ones(n), ones(1))
+
+        solver = GeNIOS.QPSolver(P, q, M, l, u);
+        GC.gc()
+        result_op = solve!(solver; options=options)
+    else
+        result_op = nothing
+    end
+
+
+    # Generic interface with custom f and g 
+    if :custom ∈ solvers
+        # f(x) = γ/2 xᵀ(FFᵀ + D)x - μᵀx
+        function f(x, F, d, μ, γ, tmp)
+            mul!(tmp, F', x)
+            qf = sum(w->w^2, tmp)
+            qf += sum(i->d[i]*x[i]^2, 1:n)
+
+            return γ/2 * qf - dot(μ, x)
+        end
+        f(x) = f(x, F, d, μ, γ, zeros(k))
+
+        #  ∇f(x) = γ(FFᵀ + D)x - μ
+        function grad_f!(g, x, F, d, μ, γ, tmp)
+            mul!(tmp, F', x)
+            mul!(g, F, tmp)
+            @. g += d*x
+            @. g *= γ
+            @. g -= μ
+            return nothing
+        end
+        grad_f!(g, x) = grad_f!(g, x, F, d, μ, γ, zeros(k))
+        Hf = HessianMarkowitz(F, d, zeros(k))
+
+        # g(z) = I(z)
+        function g(z)
+            T = eltype(z)
+            return all(z .>= zero(T)) && abs(sum(z) - one(T)) < 1e-6 ? 0 : Inf
+        end
+        function prox_g!(v, z, ρ)
+            z_max = maximum(w->abs(w), z)
+            l = -z_max - 1
+            u = z_max
+
+            # bisection search to find zero of F
+            while u - l > 1e-8
+                m = (l + u) / 2
+                if sum(w->max(w - m, zero(eltype(z))), z) - 1 > 0
+                    l = m
+                else
+                    u = m
+                end
+            end
+            ν = (l + u) / 2
+            @. v = max(z - ν, zero(eltype(z)))
+            return nothing
+        end
+
+        solver = GeNIOS.GenericSolver(
+            f, grad_f!, Hf,         # f(x)
+            g, prox_g!,             # g(z)
+            I, zeros(n)             # M, c: Mx - z = c
+        )
+        GC.gc()
+        result_custom = solve!(solver; options=options)
+    else
+        result_custom = nothing
+    end
+
+    # save data 
+    save(savefile,
+        "result_qp", result_qp,
+        "result_op", result_op,
+        "result_custom", result_custom,
+        "result_cosmo_indirect", result_cosmo_indirect,
+        "result_cosmo_direct", result_cosmo_direct,
+        "result_osqp", result_osqp,
+    )
+
+    GC.gc()
     return nothing
 end
 
-# compile
-solve!(GeNIOS.GenericSolver(f, grad_f!, Hf, g, prox_g!, I, zeros(n)); options=GeNIOS.SolverOptions(max_iters=2))
+ns = [1_000, 2_000, 4_000, 8_000, 16_000, 32_000, 64_000, 128_000]
+if !RAN_TRIALS
+    run_trial(100)
+    for n in ns[1:4]
+        solvers = n <= 32_000 ? [:qp, :op, :custom, :cosmo_indirect, :cosmo_direct, :osqp] : [:custom, :osqp, :cosmo_direct]
+        run_trial(n; solvers=solvers)
+        @info "Finished with n=$n"
+    end
+end
 
-solver = GeNIOS.GenericSolver(
-    f, grad_f!, Hf,         # f(x)
-    g, prox_g!,             # g(z)
-    I, zeros(n)             # M, c: Mx + z = c
-)
-GC.gc()
-result_custom = solve!(solver; options=options)
-
-# Save data
-save(SAVEFILE,
-    "result_qp", result_qp,
-    "result_op", result_op,
-    "result_custom", result_custom,
-)
 
 
 ## Load data from save file
-result_qp, result_op, result_custom = load(SAVEFILE,
-    "result_qp", "result_op", "result_custom",
-)
+function get_logs(n)
+    savefile = joinpath(SAVEPATH, "portfolio-genios-$n.jld2")
+    r_qp, r_op, r_custom, rc_indirect, rc_direct, r_osqp = 
+        load(savefile, 
+            "result_qp", "result_op", "result_custom",
+            "result_cosmo_indirect", "result_cosmo_direct",
+            "result_osqp"
+        )    
+    return [
+        (:qp, r_qp),
+        (:op, r_op),
+        (:custom, r_custom),
+        (:cosmo_indirect, rc_indirect),
+        (:cosmo_direct, rc_direct),
+        (:osqp, r_osqp),
+    ]
+end
 
-results = [result_qp, result_op, result_custom]
-any(x.status != :OPTIMAL for x in results) && @warn "Some problems not solved!"
+function get_timing(logs)
+    setup_times = Vector{Float64}(undef, 0)
+    solve_times = Vector{Float64}(undef, 0)
+
+    for (name, log) in logs
+        if name ∈ [:qp, :op, :custom] && !isnothing(log) && r_qp.status == :OPTIMAL
+            push!(setup_times, log.log.setup_time)
+            push!(solve_times, log.log.solve_time)
+        elseif name ∈ [:cosmo_indirect, :cosmo_direct] && !isnothing(log) && rc_indirect.status == :Solved
+            push!(setup_times, log.times.setup_time)
+            push!(solve_times, log.times.iter_time + log.times.factor_update_time)
+        elseif name == :osqp && !isnothing(log) && r_osqp.info.status == :Solved
+            push!(setup_times, log.info.setup_time)
+            push!(solve_times, log.info.solve_time)
+        else
+            push!(setup_times, NaN)
+            push!(solve_times, NaN)
+        end
+    end
+
+    return setup_times, solve_times
+end
+
+
+timings = zeros(length(ns), 6)
+setup_times = zeros(length(ns), 6)
+solve_times = zeros(length(ns), 6)
+for (i, n) in enumerate(ns)
+    logs = get_logs(n)
+    setup_time, solve_time = get_timing(logs)
+    timings[i,:] .= setup_time .+ solve_time
+    setup_times[i,:] .= setup_time
+    solve_times[i,:] .= solve_time
+end
 
 ## Plots! Plots! Plots!
-log_qp = result_qp.log
-log_op = result_op.log
-log_custom = result_custom.log
-
-# print timinigs
-print_timing("QPSolver", log_qp)
-print_timing("QPSolver, custom operators", log_op)
-print_timing("GenericSolver", log_custom)
-
-print_timing_table(
-    ["QPSolver", "QPSolver, custom operators", "GenericSolver"], 
-    [log_qp, log_op, log_custom]
-)
-
-rp_iter_plot = plot(; 
+timing_plt = plot(
+    ns, 
+    timings, 
+    yaxis=:log, 
+    xaxis=:log,
+    label=["GeNIOS (eq qp)" "GeNIOS (cust ops)" "GeNIOS (GenericSolver)"  "COSMO (indirect)" "COSMO (direct)" "OSQP"], 
+    xlabel=L"Problem size $n$", 
+    ylabel="Total solve time (s)", 
+    legend=:bottomright,
+    lw=2,
+    markershape=[:circle :diamond :utriangle :star5 :square :cross],
     dpi=300,
-    legendfontsize=10,
-    labelfontsize=14,
-    yaxis=:log,
-    ylabel=L"Primal Residual $\ell_2$ Norm",
-    xlabel="Time (s)",
-    legend=:topright,
+    yticks=[1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3],
 )
-add_to_plot!(rp_iter_plot, log_qp.iter_time, log_qp.rp, "QPSolver", :coral);
-add_to_plot!(rp_iter_plot, log_op.iter_time, log_op.rp, "QPSolver, custom ops", :purple);
-add_to_plot!(rp_iter_plot, log_custom.iter_time, log_custom.rp, "GenericSolver", :red);
-rp_iter_plot
-
-rd_iter_plot = plot(; 
-    dpi=300,
-    legendfontsize=10,
-    labelfontsize=14,
-    yaxis=:log,
-    ylabel=L"Dual Residual $\ell_2$ Norm",
-    xlabel="Time (s)",
-    legend=:topright,
-)
-add_to_plot!(rd_iter_plot, log_qp.iter_time, log_qp.rd, "QPSolver", :coral);
-add_to_plot!(rd_iter_plot, log_op.iter_time, log_op.rd, "QPSolver, custom ops", :purple);
-add_to_plot!(rd_iter_plot, log_custom.iter_time, log_custom.rd, "GenericSolver", :red);
-rd_iter_plot
-
-# pstar = log_high_precision.obj_val[end]
-# obj_val_iter_plot = plot(; 
-#     dpi=300,
-#     legendfontsize=10,
-#     labelfontsize=14,
-#     yaxis=:log,
-#     ylabel=L"$(p-p^\star)/p^\star$",
-#     xlabel="Time (s)",
-#     legend=:topright,
-# )
-# add_to_plot!(obj_val_iter_plot, log_pc.iter_time[2:end], (log_pc.obj_val[2:end] .- pstar) ./ pstar, "GeNIOS", :coral);
-# add_to_plot!(obj_val_iter_plot, log_npc.iter_time[2:end], (log_npc.obj_val[2:end] .- pstar) ./ pstar, "No PC", :purple);
-# add_to_plot!(obj_val_iter_plot, log_exact.iter_time[2:end], (log_exact.obj_val[2:end] .- pstar) ./ pstar, "ADMM (pc)", :red);
-# add_to_plot!(obj_val_iter_plot, log_exact_npc.iter_time[2:end], (log_exact_npc.obj_val[2:end] .- pstar) ./ pstar, "ADMM (no pc)", :mediumblue);
-# obj_val_iter_plot
-
-savefig(rp_iter_plot, joinpath(FIGS_PATH, "5-portfolio-rp-iter.pdf"))
-savefig(rd_iter_plot, joinpath(FIGS_PATH, "5-portfolio-rd-iter.pdf"))
-# savefig(obj_val_iter_plot, joinpath(FIGS_PATH, "5-portfolio-obj-val-iter.pdf"))
+savefig(timing_plt, joinpath(FIGS_PATH, "5-portfolio-timing.pdf"))
